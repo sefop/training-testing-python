0eadfa56a70c244790308b78302cb557bde4e36f
fzentenosmith <zenteno.fco@gmail.com>
2026-06-21 22:53:06 -0500

    divide ready

--- tests/test_calculator_divide.py
+++ tests/test_calculator_divide.py
@@ -13,6 +13,21 @@ def test__divide__given_two_valid_numbers__returns_their_quotient() -> None:
     # Assert
     pass
 
+def test__divide__given_dividend_and_one__returns_dividend() -> None:
+    # Arrange
+
+    # Act
+
+    # Assert
+    pass
+
+def test__divide__given_same_numbers__returns_one() -> None:
+    # Arrange
+
+    # Act
+
+    # Assert
+    pass
 
 def test__divide__given_int_operands__returns_float() -> None:
     # Arrange
@@ -22,7 +37,6 @@ def test__divide__given_int_operands__returns_float() -> None:
     # Assert
     pass
 
-
 def test__divide__given_invalid_types__raises_type_error() -> None:
     # Arrange
 
@@ -30,7 +44,6 @@ def test__divide__given_invalid_types__raises_type_error() -> None:
 
     # Assert
     pass
-
 
 def test__divide__given_non_finite_operand__raises_value_error() -> None:
     # Arrange
@@ -40,7 +53,6 @@ def test__divide__given_non_finite_operand__raises_value_error() -> None:
     # Assert
     pass
 
-
 def test__divide__given_zero_divisor__raises_zero_division_error() -> None:
     # Arrange
 
@@ -48,7 +60,6 @@ def test__divide__given_zero_divisor__raises_zero_division_error() -> None:
 
     # Assert
     pass
-
 
 def test__divide__given_inputs_that_overflow__raises_overflow_error() -> None:
     # Arrange
